6a3afd33c39b0257c86f6ba3354e2e1e1c208c0e
Errol Rougier <errolrougier@dyn-160-39-244-251.dyn.columbia.edu>
2019-01-19 14:09:17 -0500

    server running linked routes

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
-var express = require('express')
-var app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+//require express
+var express = require("express");
+
+//create and instance of express
+var app = express();
+
+
+
+//what port will it be on
+var PORT = process.env.PORT || 8080;
+
+console.log("This node is running");
+
+
+
+
+
+//
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+
+  // Log (server-side) when our server has started
+console.log("Server listening on http://localhost:" + PORT);
+
+});
