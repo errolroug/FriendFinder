614427ddb34527716071a6f5a73b5e8098f7e0ec
Errol Rougier <errolrougier@Errols-MBP.fios-router.home>
2019-03-27 10:52:57 -0400

    Files for friends finder

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,7 +10,49 @@ app.get("/api/friends", function(req, res) {
 
 
 app.post("/api/friends", function(req,res){
-    res.json()
-});
+  
+  
+  
+  //Takes the input as an object
+
+   var newUser = req.body;
+   var newScores = newUser.scores;
+   var bestFriend = {
+     name:"",
+     photo:"",
+     score:[""],
+     friendDifference:""
+   };
+
+//add the new User to the friends module
+   friends.push(newUser);
+
+  
+
+//first loop for every user
+   for(var i = 0; i < friends.length; i++){
+     var currentFriend = friends[i];
+     //for every friend in the database loop and compare individual scores
+        for(var j = 0; j < currentFriend.scores.length;j++){
+          //this loops through the arrays and grabs that specific index
+          var currentFriendScores = currentFriend.scores[j];
+            var currentUserScores = newScores[j];
+
+            //calculate the difference
+
+           var scoreDifference = Math.abs(parseInt(currentUserScores) - parseInt(currentFriendScores));
+        }
+     
+     //if the difference is the least, adjust data in object
+        if (scoreDifference <= bestFriend.scoreDifference) {
+          // Reset the bestMatch to be the new friend.
+          bestFriend.name = currentFriend.name;
+          bestFriend.photo = currentFriend.photo;
+          bestFriend.score = currentFriend.score
+          bestFriend.friendDifference = totalDifference;
+          };
+
+    }
+  });
 
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,7 +3,7 @@ var path = require("path");
 
 module.exports = function(app){
 
-
+// this will go to survey and any other path will hit home.html
 app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
